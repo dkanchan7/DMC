4b938a3bbaf5a4568c9a35bb884a1cf69f34c9ac
dkanchan7 <160234282+dkanchan7@users.noreply.github.com>
2024-05-23 19:32:45 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,126 +1,161 @@
-import conceptnet_lite
-from conceptnet_lite import Label, edges_for
-from nrclex import NRCLex
-from better_profanity import profanity
-import openai
-import random
+from flask import Flask, flash, redirect,render_template, request,jsonify
+import os
+from together import Together
+import re
+import json
+from transformers import pipeline
 
-conceptnet_lite.connect("")
 
-def get_emotion_words(word, emotion):
-    # list of words to return
-    emotion_words = []
-    no_emotions = []
+app = Flask(__name__)  
+  
 
-    # list of relations to search
-    relations = ['related_to','form_of','is_a','part_of','used_for','capable_of','causes','has_subevent','has_first_subevent','has_prerequisite','has_property','synonym','antonym','distinct_from','derived_from','defined_as','has_context','similar_to','etymologically_related_to','receives_action']
+client = Together(api_key="09cbbbbd5c5096e7f7fb35bbcbea9efdf4cb47a611337bcc166b7edd6046a8cb")
 
-    # check for profanity in word
-    if profanity.contains_profanity(word) == True:
-        print('Sorry, there is no information for this word')
-        return []
+url = "https://api.together.xyz/inference"
 
-    # get associated words
-    usecases = []
-    for i in relations:
-        for e in edges_for(Label.get(text = word, language ='en').concepts, same_language = True):
-            if(e.relation.name == i and e.start.text == word):
-                if e.end.text not in usecases and profanity.contains_profanity(e.end.text) == False:
-                    usecases.append(e.end.text.replace('_',' '))
+Model="meta-llama/Llama-3-70b-chat-hf"
 
-        for e in edges_for(Label.get(text = word, language ='en').concepts, same_language = True):
-            if(e.relation.name == i and e.end.text == word):
-                if e.start.text not in usecases and profanity.contains_profanity(e.start.text) == False:
-                    usecases.append(e.start.text.replace('_',' '))
+ 
+model_ckpt = "abhyast/minilm-finetuned-emotion-class-model"
+pipe = pipeline("text-classification", model=model_ckpt)
 
-    # check emotion of each word
-    for word in usecases:
-        emo_dict = NRCLex(word).affect_frequencies
 
-        # add word to emotion_words list if it has the specified emotion
-        if emotion in emo_dict.keys() and emo_dict[emotion] > 0:
-            emotion_words.append(word)
-        else:
-            no_emotions.append(word) 
+def detect_emotion(text):
+    results = pipe(text)
+    # Assuming we always get at least one result, extract the first one
+    return results[0] if results else None
+
+
+ 
+
+
+@app.route('/')  
+def index():  
+    return render_template('index.html')  
+
+def create_ad_slogan(pd,emotion):
+    prompt = '''You are an creative Advertisement generator given the user emotions and product details.
+    You have to generate very creative and attractive advertisement slogan so that it can market well and also you have to generate interesting Advertisement content/description
     
-    if len(emotion_words) == 0:
-        return no_emotions
-    else:
-        return emotion_words
-
-
-
-# def generate_story(prompt, character, emotion):
-#     # set up API client
-#     openai.api_key = "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"
-def generate_story(prompt, character, emotion):
-    # Replace this with your own logic to generate stories based on the prompt, character, and emotion
-    story_template = f"This is a story about {character} experiencing {emotion} while dealing with {prompt}."
-    return story_template
-
-
-    # generate story
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=f"#prompt : pen #idea : A student is in an examination hall. He starts imagining his family members pressurising him to get good marks. Once he opens his pencil box and hold the pen, he feels confident and writes the exam really well. This depicts how a good pen that offers comfort while writing can ease the students pressures while writing an exam. #prompt : Air Conditioning #idea : A couple has a discussion on buying an Air conditioning. Wife buys the Air Conditioning without discussing with him. Husband advices her that she could have done some research before buying one. Then wife explains all the features the Air Conditioning provides which makes it best in the market. #prompt : Chocolate #idea : A small girl enters a grocery store with her mother. The mother looks tensed and worried while speaking to someone on the phone. The girl goes to the shop keeper and asks for a chocolate. She gives her toys one by one as money. The shop keeper gives her the chocolate and in return gives a small toy as change. The girl gives the chocolate to her mother and wishes Happy Birthday!. This makes the mother feel happy and hugs her daughter. #prompt : Bulb #idea : Husband and wife talk to each other in the video call. Husband is on an office trip. He gets a call from his boss and asks his wife to stay on line. While he is speaking to his boss he notices that his wife fell asleep. So he switches the lights off in his house with the help his mobile phone. This shows how smart bulbs are helpful. #prompt: Television #idea: A family of four is gathered around their television in the living room. They are watching a movie and eating popcorn. The youngest member of the family is so absorbed in the show that he hardly notices his siblings passing him popcorn and making funny comments about the actors. This shows how television brings an entire family together for some quality time. #prompt: Flower Vase #idea: A woman is having a tough day at work and is feeling low. She decides to take a break and goes to the nearby florist. She buys a beautiful flower vase and brings it home. She looks at the vase and smiles. She arranges the flowers in the vase to create a stunning bouquet. Looking at the vase and the flowers, she feels relaxed and happier. This illustrates how flowers in a vase can bring joy and peace to someone's day.#prompt: Photo frame #idea: A family is gathered together to celebrate the grandmother's 80th birthday. As a surprise, the family presents her with a photo frame. As grandmother looks at the frame, she starts reminiscing about the times spent with her family. This shows how a photo frame can evoke fond memories and bring joy to the elderly. #prompt: Suitcase as a bomb #idea: A young girl is travelling alone on a bus. She notices an unattended suitcase in the corner of the bus and gets suspicious. She immediately alerts the authorities who check the suitcase and find it to be a bomb. This illustrates how even the most innocuous objects can be used as a weapon of terror. #prompt: Travelling in a flight #idea: A family is travelling to another city in a flight. They are all excited to reach the destination and explore the new city. The family is looking out of the window and the little ones are asking questions about the birds and the clouds. This shows how travelling in a flight can be fun and exciting for the entire family.#prompt: Cool drink in Christmas celebration #idea: It is Christmas celebration and a family is gathered around the Christmas tree. They open their presents and enjoy the delicious Christmas feast. The little ones ask for a cool drink and the father brings out a can of soda from the refrigerator. The family cheers and enjoy the festive season with a cool drink in hand. This illustrates how a simple thing like a cool drink can add joy to a special occasion. #prompt: Generate a story advertising {prompt} with an emotion of {emotion} with charateristics of {character}",
-        temperature=0.7,
-        max_tokens=370,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
+    '''
+   
+    
+    fewshots = ''' Only respond in below mention output format
+             {"ad_slogan": "Generated Ad slogan", "ad_description": "Generated Ad description"}
+    
+    Product details and user review based emotion given below:
+         '''
+    inputs="Product details :" + pd + " User emotion: "+ emotion
+    prompt = prompt +   fewshots + inputs
+    response = client.chat.completions.create(
+    model=Model,
+    messages=[{"role": "user", "content": prompt}],
     )
-
-    # get generated text
-    story = response["choices"][0]["text"]
-
-    # return first 200 words of story
-    return story
-
-def get_story(word, emotion):
-    stories = []
-    words = get_emotion_words(word, emotion)
-    if len(words) > 10:
-        ten_words = random.sample(words, 10)
-        for i in ten_words:
-            stories.append(generate_story(word, emotion, i))
-    else:
-        for i in words:
-            stories.append(generate_story(word, emotion, i))
-    # if len(emotion_words) == 0:
-    #     if len(no_emotion_words) > 10:
-    #         #If there are no emotion words and no emotion words have a number greater than 10
-    #         ten_no_emotion_words = random.sample(no_emotion_words, 10)
-    #         for i in ten_no_emotion_words:
-    #             stories.append(generate_story(word, emotion, i))
-    #     else :
-    #         #If there are no emotion words and no emotion words have a number less than 10
-    #         for i in no_emotion_words:
-    #             stories.append(generate_story(word, emotion, i))
-    # else:
-    #     if len(emotion_words) > 10:
-    #         #If there are emotion words and emotion words have a number greater than 10
-    #         ten_emotion_words = random.sample(emotion_words, 10)
-    #         for i in ten_emotion_words:
-    #             stories.append(generate_story(word, emotion, i))
-    #     else:
-    #         #If there are emotion words and emotion words have a number less than 10
-    #         for i in emotion_words:
-    #             stories.append(generate_story(word, emotion, i))
-
-    return stories
-
-
-
-if __name__ == '__main__':
-    word = input("Enter a word: ")
-    emotion = input("Enter an emotion: ")
+    print("response :\n",response.choices[0].message.content)
+    res = response.choices[0].message.content
+    res_dict = extract_json(res)  
     
-    output_story = get_story(word, emotion)
-    output_words = get_emotion_words(word, emotion)
     
-    print("Output Story:")
-    for story in output_story:
-        print(story)
+    # Extract individual response values  
+    adSlogan = res_dict["ad_slogan"]  
+    ad_description = res_dict["ad_description"]
+   
+    return  adSlogan,ad_description
 
-    print("\nOutput Words:")
-    print(output_words)
+
+
+def extract_json(response):  
+    json_match = re.search(r'\{.*\}', response, re.DOTALL)  
+    if json_match:  
+        json_string = json_match.group(0)  
+        json_data = json.loads(json_string)  
+        return json_data  
+    else:  
+        return None 
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    if request.method =="POST":
+ 
+        user_review = request.form['user_review']
+        prod_details =request.form['product_details']
+        emotion = detect_emotion(user_review)
+        emotion = emotion['label']
+        print("emotion",emotion)
+        adSlogan,ad_description = create_ad_slogan(prod_details,emotion)
+          
+    return render_template("prod.html", adSlogan=adSlogan,ad_description=ad_description,emotion=emotion)
+
+
+
+# def create_product_title_desc(text):  
+#     # prompt = f"Please provide clear, concise, and informative summary of the following news article in no more than {sumwordcount} words. Include the main points, key details, and any relevant context to ensure a comprehensive understanding of the content. Please ensure the generated summary should not be more than {sumwordcount} words ."  
+#     prompt ='''
+#     Generate a catchy marketing product headline, killer product slogan for marketing highlight brand awareness and product description for the product details given below. Generate the output in proper json format in which first key value pair contains the headline and its generated headline, second key value pair contains description. As shown below
+# {"headline" : "generated headline1, "slogan": generated slogan"} \n
+#     '''
+#     content = prompt + " " + "product: "+ text
+#     return generate_text(project_id, model_name, temperature,max_decode_steps,top_p,top_k, content,location)
+    
+# def create_product_feats(text):  
+#     # prompt = f"Please provide clear, concise, and informative summary of the following news article in no more than {sumwordcount} words. Include the main points, key details, and any relevant context to ensure a comprehensive understanding of the content. Please ensure the generated summary should not be more than {sumwordcount} words ."  
+#     prompt ='''
+#     Generate 3 killer features for the product given below numbered from 1 to 3. Generate the whole content in less than 200 words only. \n
+#     '''
+#     content = prompt + " " + "product: "+ text
+#     return generate_text(project_id, model_name, temperature,max_decode_steps,top_p,top_k, content,location)
+
+# def create_insta_tags(text):  
+#     # prompt = f"Please provide clear, concise, and informative summary of the following news article in no more than {sumwordcount} words. Include the main points, key details, and any relevant context to ensure a comprehensive understanding of the content. Please ensure the generated summary should not be more than {sumwordcount} words ."  
+#     prompt ='''
+#     Generate three hashtags in one line for the below product in instagram for young generation. :\n
+#     '''
+#     content = prompt + " " + text
+#     return generate_text(project_id, model_name, 0.6,max_decode_steps,top_p,top_k, content,location)
+
+# def create_twitter_tags(text):  
+#     # prompt = f"Please provide clear, concise, and informative summary of the following news article in no more than {sumwordcount} words. Include the main points, key details, and any relevant context to ensure a comprehensive understanding of the content. Please ensure the generated summary should not be more than {sumwordcount} words ."  
+#     prompt ='''
+#     Generate three hashtags in one line for the below product in twitter for young generation. :\n
+#     '''
+#     content = prompt + " " + text
+#     return generate_text(project_id, model_name, 0.6,max_decode_steps,top_p,top_k, content,location)
+
+# def create_facebook_tags(text):  
+#     # prompt = f"Please provide clear, concise, and informative summary of the following news article in no more than {sumwordcount} words. Include the main points, key details, and any relevant context to ensure a comprehensive understanding of the content. Please ensure the generated summary should not be more than {sumwordcount} words ."  
+#     prompt ='''
+#     Generate three hashtags in one line for the below product in facebook for young generation. :\n
+#     '''
+#     content = prompt + " " + text
+#     return generate_text(project_id, model_name, 0.6,max_decode_steps,top_p,top_k, content,location)
+
+# def create_insta_posts(text):  
+#     # prompt = f"Please provide clear, concise, and informative summary of the following news article in no more than {sumwordcount} words. Include the main points, key details, and any relevant context to ensure a comprehensive understanding of the content. Please ensure the generated summary should not be more than {sumwordcount} words ."  
+#     prompt ='''
+#     Generate Short product slogan in 10 words to attract young generation for marketing below mentioned product in instagram. Dont explain the slogan.\n
+#     '''
+#     content = prompt + " " + text
+#     return generate_text(project_id, model_name, 0.6,max_decode_steps,top_p,top_k, content,location)
+
+# def create_fb_posts(text):  
+#     # prompt = f"Please provide clear, concise, and informative summary of the following news article in no more than {sumwordcount} words. Include the main points, key details, and any relevant context to ensure a comprehensive understanding of the content. Please ensure the generated summary should not be more than {sumwordcount} words ."  
+#     prompt ='''
+#         Generate Short product slogan in 10 words to attract young generation for marketing below mentioned product in facebook.Dont explain the slogan. :\n
+#         '''
+#     content = prompt + " " + text
+#     return generate_text(project_id, model_name, 0.6,max_decode_steps,top_p,top_k, content,location)
+
+# def create_twitter_posts(text):  
+#     # prompt = f"Please provide clear, concise, and informative summary of the following news article in no more than {sumwordcount} words. Include the main points, key details, and any relevant context to ensure a comprehensive understanding of the content. Please ensure the generated summary should not be more than {sumwordcount} words ."  
+#     prompt ='''
+#     Generate Short product slogan in 10 words to attract young generation for marketing below mentioned product in twitter.Dont explain the slogan.\n
+#     '''
+#     content = prompt + " " + text
+#     return generate_text(project_id, model_name, 0.6,max_decode_steps,top_p,top_k, content,location)
+
+
+
+
+  
+if __name__ == '__main__':  
+    app.run(host='localhost', port='8888', debug=True)  
